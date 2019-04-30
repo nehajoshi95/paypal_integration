7dfb3a006f197b11b70aab031005b2923886135b
nehajoshi95 <neha95joshi@gmail.com>
2019-04-30 17:25:34 +0530

    last comit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root 'products#new'
 	resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
